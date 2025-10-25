8ac57ea835bf67af28a0f6e6e715191d75b006ce
ErtanSarihan <iertansa@gmail.com>
2025-10-25 18:36:39 +0300

    Now HammerHitController is responsible for controlling the player hit buttons, player cannot use the left mouse click or space bar back to back, player should alternate between buttons on each hit

--- Assets/Game/_Scripts/HammerHitController.cs
+++ Assets/Game/_Scripts/HammerHitController.cs
@@ -4,12 +4,40 @@ using UnityEngine.InputSystem;
 
 namespace Game._Scripts {
   public class HammerHitController : MonoBehaviour {
-
     public static event Action OnHammerHit;
+    public static event Action OnRepeatingButtonsHit;
+
+    private enum HitButton {
+      None,
+      Mouse,
+      Space
+    }
+
+    private HitButton _lastButtonPressed = HitButton.None;
 
     private void Update() {
-      if (Mouse.current.leftButton.wasPressedThisFrame || Keyboard.current.spaceKey.wasPressedThisFrame) {
-        OnHammerHit?.Invoke();
+      bool mousePressed = Mouse.current.leftButton.wasPressedThisFrame;
+      bool spacePressed = Keyboard.current.spaceKey.wasPressedThisFrame;
+
+      if (mousePressed) {
+        if (_lastButtonPressed == HitButton.Mouse) {
+          OnRepeatingButtonsHit?.Invoke();
+          // Debug.Log("Repeating buttons hit: mouse left clicked");
+        }
+        else {
+          _lastButtonPressed = HitButton.Mouse;
+          OnHammerHit?.Invoke();
+        }
+      }
+      else if (spacePressed) {
+        if (_lastButtonPressed == HitButton.Space) {
+          OnRepeatingButtonsHit?.Invoke();
+          // Debug.Log("Repeating buttons hit: space clicked");
+        }
+        else {
+          _lastButtonPressed = HitButton.Space;
+          OnHammerHit?.Invoke();
+        }
       }
     }
   }
